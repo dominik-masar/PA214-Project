f985bb250000eb26a10aab7cb04af812c147194a
Veronika Borýsková <xborysk@fi.muni.cz>
2025-05-25 21:30:53 +0200

    merging main and mainDash

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import pandas as pd
 from visualisation.barchartFigure import barchart_fig
 from visualisation.mapGeo import map_fig
+from visualisation.bottomView import get_bottom_fig #TODO rename
 
 DATASET_FILENAME = "datasets/space_with_geo_with_countries.csv"
 
@@ -11,11 +12,13 @@ DATASET_FILENAME = "datasets/space_with_geo_with_countries.csv"
 df = pd.read_csv(DATASET_FILENAME)
 
 # Create figures
-main_fig = map_fig()
+main_fig = map_fig(df)
 bar_fig = barchart_fig(df, 2000) # TODO: insert variable year
+active_years_fig = get_bottom_fig()  # Default: astronauts from U.S.
 
 main_fig.update_layout(height=700)
 bar_fig.update_layout(height=700)
+active_years_fig.update_layout(height=700)
 
 # Initialize Dash app
 app = dash.Dash(__name__)
@@ -23,7 +26,8 @@ app = dash.Dash(__name__)
 # Layout with 80:20 ratio
 app.layout = html.Div([
     html.Div([dcc.Graph(figure=main_fig)], style={'width': '70%', 'display': 'inline-block'}),
-    html.Div([dcc.Graph(figure=bar_fig)], style={'width': '30%', 'display': 'inline-block'})
+    html.Div([dcc.Graph(figure=bar_fig)], style={'width': '30%', 'display': 'inline-block'}),
+    html.Div([dcc.Graph(figure=active_years_fig)])
 ])
 
 if __name__ == '__main__':
